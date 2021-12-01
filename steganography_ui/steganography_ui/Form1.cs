512abaa7c8e74e8538f4787e40b0d23af36799a9
aleasper <alexidest@gmail.com>
2021-12-02 00:33:47 +0300

    Add example of working with C# interface

--- steganography_ui/steganography_ui/Form1.cs
+++ steganography_ui/steganography_ui/Form1.cs
@@ -14,6 +14,11 @@ namespace steganography_ui
 {
     public partial class Form1 : Form
     {
+
+
+        string file;
+        string txtFile;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,9 +26,18 @@ namespace steganography_ui
 
         private void button1_Click(object sender, EventArgs e)
         {
+ 
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = workingDirectory;
+
+
+            Console.WriteLine(projectDirectory);
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = "C:\\projects\\steganography\\venv\\Scripts\\python.exe";
-            start.Arguments = string.Format("{0} {1}", "C:\\projects\\steganography\\main.py", "a b c");
+            start.FileName = string.Format("{0}\\venv\\Scripts\\python.exe", projectDirectory);
+            start.Arguments = string.Format(
+                "{0} {1}", string.Format("{0}\\main.py", projectDirectory),
+                this.file + " " + txtFile + " " + "ouput.wav"
+                );
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
             using (Process process = Process.Start(start))
@@ -32,10 +46,59 @@ namespace steganography_ui
                 {
                     string result = reader.ReadToEnd();
                     Console.Write(result);
-                    
+
                 }
                 Console.Write(process.ExitCode);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Audio files |*.wav";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                this.file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text files | *.txt";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                this.txtFile = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
         }
     }
 }
